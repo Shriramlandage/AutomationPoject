a9b1a63653163f053819bbe8533d312202ebe555
Shriramlandage <sriramlandge44@gmail.com>
2024-01-25 18:45:48 +0530

    new file

--- TestNgProject/src/pacticeTestNg/PS1.java
+++ TestNgProject/src/pacticeTestNg/PS1.java
@@ -2,16 +2,15 @@ package pacticeTestNg;
 
 import org.testng.annotations.Test;
 
-public class PS1 {
+public class PS1 extends PS {
 	
 	@Test
 	public void testRun() {
 		PS2 ps2=new PS2(3);
 		
 		int a =3;
-		dothis();
+		doThis();
 		System.out.println(ps2.increment());
 		System.out.println(ps2.decrement());
 	}
-
 }
